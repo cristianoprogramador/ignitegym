205cc116dd6ab58abfec7d68517eb241fb48bc5c
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2022-10-25 22:17:17 -0300

    Rota entre as telas de Login foram criadas

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { Loading } from '@components/Loading';
 import {THEME} from './src/theme'
 import { SignIn } from '@screens/SignIn';
 import { SignUp } from '@screens/SignUp';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,7 +19,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ? <SignUp /> : <Loading />}
+      { fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,19 +1,29 @@
+import { useNavigation } from '@react-navigation/native'
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
 
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
+
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
 export function SignUp() {
+
+  const navigation = useNavigation()
+
+  function handleGoBack(){
+    navigation.goBack()
+  }
+
   return (
     <ScrollView contentContainerStyle=
       {{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} bg='gray.700' px={10} pb={16}>
+      <VStack flex={1}  px={10} pb={16}>
         <Image
           source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt='Pessoas treinando'
           resizeMode='contain'
           position='absolute'
@@ -52,9 +62,10 @@ export function SignUp() {
         </Center>
 
         <Button
-          title='Criar conta'
+          title='Voltar para tela de Login'
           variant='outline'
           mt={20}
+          onPress={handleGoBack}
         />
       </VStack>
     </ScrollView>
