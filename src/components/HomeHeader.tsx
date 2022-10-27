35120f6b1e7d99f0efc2d56adb1b43ff806d0bef
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2022-10-26 22:15:23 -0300

    Tabela de exercicios criada

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,32 @@
+import { Heading, HStack, Text, VStack, Icon } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { UserPhoto } from "./usePhoto";
+import { TouchableOpacity } from "react-native";
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} px={8} pb={5} alignItems="center">
+      <UserPhoto
+        source={{
+          uri: "https://avatars.githubusercontent.com/u/102186472?v=4",
+        }}
+        alt="Imagem do usuario"
+        mr={4}
+        size={16}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Cristiano
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,67 @@
-import {Center, Text} from 'native-base'
+import { ExerciseCard } from "@components/ExerciseCard";
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
+import { Center, HStack, Text, VStack, FlatList, Heading } from "native-base";
+import { useState } from "react";
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState("costas");
+  const [groups, setGroups] = useState([
+    "costas",
+    "ombros",
+    "biceps",
+    "triceps",
+  ]);
+  const [exercises, setExercises] = useState([
+    "Puxada Frontal",
+    "Remada unilateral",
+    "Levantamento de peso",
+    "Remada Curvada",
+  ]);
+
   return (
-    <Center flex={1}>
-      <Text color='white'>Home</Text>
-    </Center>
-  )
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={
+              String(groupSelected).toLocaleUpperCase() ===
+              String(item).toLocaleUpperCase()
+            }
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercicios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
+    </VStack>
+  );
 }
